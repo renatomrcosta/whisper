5e505e2653b5474b3071148ad1a79e0084a5a60e
Renato Costa <renato.costa@cipal.be>
2018-07-05 22:19:05 +0200

    Tried testing the goddamn application. It failed

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,6 +3,7 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { MaterialWrapperModule } from "./material-wrapper/material-wrapper.module";
 
 
 @NgModule({
@@ -10,7 +11,8 @@ import { AppComponent } from './app.component';
     AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    MaterialWrapperModule
   ],
   providers: [],
   bootstrap: [AppComponent]
