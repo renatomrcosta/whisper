4efc26fde69aa979ee2f86f212a28f6a83b1b144
Renato Costa <renato.costa@cipal.be>
2018-07-09 19:20:55 +0200

    Added module and component for sign-in

---
+++ src/app/sign-in/sign-in.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { SignInComponent } from './sign-in.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [SignInComponent]
+})
+export class SignInModule { }
