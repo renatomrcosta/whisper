0226e1a8d01d58ac777e15fc1b8802f5a8670bb2
Renato Costa <renato.costa@cipal.be>
2018-07-10 19:33:58 +0200

    Added prototypical component

---
+++ src/app/messages/messages.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { MessagesComponent } from './messages.component';
+
+@NgModule({
+  imports: [
+    CommonModule
+  ],
+  declarations: [MessagesComponent]
+})
+export class MessagesModule { }
