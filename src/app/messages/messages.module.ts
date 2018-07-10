b33fa78d78a7f1cb057a60bc2271395bf392f526
Renato Costa <renato.costa@cipal.be>
2018-07-10 19:37:30 +0200

    Imported necessary modules in the messages module

--- src/app/messages/messages.module.ts
+++ src/app/messages/messages.module.ts
@@ -1,10 +1,20 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { MessagesComponent } from './messages.component';
+import {RouterModule} from "@angular/router";
+import {Routes} from "@angular/router";
+import {AuthGuard} from "../auth/auth.guard";
+import {MaterialWrapperModule} from "../material-wrapper/material-wrapper.module";
+
+const ROUTES: Routes = [
+  {path: '/messages', component: MessagesComponent, canActivate: [AuthGuard] }
+];
 
 @NgModule({
   imports: [
-    CommonModule
+    CommonModule,
+    RouterModule.forChild(ROUTES),
+    MaterialWrapperModule
   ],
   declarations: [MessagesComponent]
 })
