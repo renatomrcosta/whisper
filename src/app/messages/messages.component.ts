51120dc26017d6655f96ca16e0790a98edbc6742
Renato Costa <renato.costa@cipal.be>
2018-07-10 20:44:02 +0200

    Added prototype collection rad / write

--- src/app/messages/messages.component.ts
+++ src/app/messages/messages.component.ts
@@ -1,15 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import {Component} from '@angular/core';
+import {Observable} from "rxjs/internal/Observable";
+import {AngularFirestore} from "angularfire2/firestore";
 
 @Component({
   selector: 'app-messages',
   templateUrl: './messages.component.html',
   styleUrls: ['./messages.component.css']
 })
-export class MessagesComponent implements OnInit {
+export class MessagesComponent{
+  messages: Observable<any[]>;
 
-  constructor() { }
-
-  ngOnInit() {
+  constructor(private db: AngularFirestore){
+    this.messages = db.collection('messages').valueChanges();
   }
 
+  addMessage(){
+    this.db.collection('messages').add({
+      datetime: new Date(),
+      roomId: '1',
+      text: 'bananinha',
+      userId: 'test'
+    });
+  }
 }
