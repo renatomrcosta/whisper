f7e12cfd02731aa10a61ccb596f148938376cfc7
Renato Costa <renato.rezende.costa@gmail.com>
2018-07-21 02:46:15 +0200

    added firebase configs

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyCRio05ViNE19LvUsaqJ7_WxTLyR_5k1hE",
+    authDomain: "whisper-861fc.firebaseapp.com",
+    databaseURL: "https://whisper-861fc.firebaseio.com",
+    projectId: "whisper-861fc",
+    storageBucket: "whisper-861fc.appspot.com",
+    messagingSenderId: "653633765236"
+  }
 };
